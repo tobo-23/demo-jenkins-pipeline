a93b405972bee19e6b3afa6f682799621fc3e127
tobo-23 <nassimlad50@gmail.com>
2025-10-30 09:28:34 +0100

    Add unit test for App.add method

---
+++ src/test/java/AppTest.java
@@ -0,0 +1,9 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class AppTest {
+    @Test
+    void testAdd() {
+        assertEquals(3, App.add(1, 2));
+    }
+}
